d02fa50a58b72e6307e77a466dcab7c0839beaec
Anca Ciascaiu <aciascaiu@cars.com>
2019-08-19 11:10:08 -0500

    add route and template for hello :index

--- hello/lib/hello_web/router.ex
+++ hello/lib/hello_web/router.ex
@@ -2,21 +2,22 @@ defmodule HelloWeb.Router do
   use HelloWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", HelloWeb do
-    pipe_through :browser
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    get("/hello", HelloController, :index)
   end
 
   # Other scopes may use custom stacks.
